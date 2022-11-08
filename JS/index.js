a1cf268abfb7af929370a96eda48794308981883
Romina Giselle Gualampa <r.gualampa@gmail.com>
2022-11-07 21:53:56 -0300

    modificación3

---
+++ JS/index.js
@@ -0,0 +1,14 @@
+var cad='
+<nav>
+    <ul class="ul"><!-- ETIQUETA EN BLOQUE -->
+        <a class="nav" href="index.html">Inicio</a>
+        <a class="nav" href="productos.html">Productos</a>
+        <a class="nav" href="capacitaciones.html">Capacitaciones</a>
+        <a class="nav" href="contacto.html">Contacto</a>
+      
+    </ul>
+</nav>'
+
+document.getElementById("idheader").innerHTML=cad;
+
+
